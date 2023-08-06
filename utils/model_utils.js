10f065042e9c79d226742b45b38574fc8cb96b93
Ankush Chavan <cankush625@gmail.com>
2023-08-06 10:45:27 +0530

    feat(Prisma Model Utils): Model util function to exclude sensitive fields like password from query response
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

---
+++ utils/model_utils.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+
+// Exclude keys from user
+function exclude(user, keys) {
+  return Object.fromEntries(
+    Object.entries(user).filter(([key]) => !keys.includes(key))
+  );
+}
+
+
+async function get_user_by_id(id) {
+    const user = await prisma.user.findUnique({ where: { id: 1 } })
+    return exclude(user, ['password']);
+}
