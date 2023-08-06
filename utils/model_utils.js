48149ae3a3a8a50b790ea842def2238c16aa58e1
Ankush Chavan <cankush625@gmail.com>
2023-08-06 11:59:45 +0530

    refactor(Code refactor): Reformat the code using prettier
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- utils/model_utils.js
+++ utils/model_utils.js
@@ -1,17 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 // Exclude keys from user
 function exclude(user, keys) {
-  return Object.fromEntries(
-    Object.entries(user).filter(([key]) => !keys.includes(key))
-  );
+    return Object.fromEntries(
+        Object.entries(user).filter(([key]) => !keys.includes(key))
+    );
 }
 
-
 async function get_user_by_id(id) {
-    const user = await prisma.user.findUnique({ where: { id: 1 } })
-    return exclude(user, ['password']);
+    const user = await prisma.user.findUnique({ where: { id: 1 } });
+    return exclude(user, ["password"]);
 }
